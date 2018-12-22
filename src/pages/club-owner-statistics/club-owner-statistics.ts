58233841c615d80731a4da20dca85da47b88962a
3amr <amr.ahmed.abdelbaqi@gmail.com>
2018-12-23 01:47:02 +0200

    bugs not fixed

--- src/pages/club-owner-statistics/club-owner-statistics.ts
+++ src/pages/club-owner-statistics/club-owner-statistics.ts
@@ -33,14 +33,31 @@ export class ClubOwnerStatisticsPage {
     console.log('ionViewDidLoad ClubOwnerStatisticsPage');
     let id = this.dataProvider.get_id();
     this.cid = id;
+    console.log("*******************clubid********************")
     console.log(this.cid)
+    console.log("***************************************")
+    
     this.ownerProvider.getHourStats(this.cid).subscribe(data => {
-      for (let d of data) this.table1.push({ hour: d['hour'], num: d['num'] })
-    });
-    this.ownerProvider.getPitchStats(this.cid).subscribe(data => {
+     if(data){
       console.log(data);
-      for (let d of data) this.table2.push({ pitchno: d['pitchno'], num: d['num'] })
+      for (let d of data) this.table1.push({ hour: d['hour'], num: d['num'] })
+      this.ownerProvider.getPitchStats(this.cid).subscribe(result => {
+        if(result)
+        {
+          console.log(result);
+          for (let d of result) this.table2.push({ pitchno: d['pitchno'], num: d['num'] })
+        
+        }
+        else {
+          this.table2.push({ pitchno: '-----', num:'--------' })
+        }
+      });
+    }
+     else {
+      this.table1.push({ hour: '-----', num:'--------' })
+     }
     });
+    console.log("***************************************")    
   }
 
 }
